49c2e71ddd358855957efda2a3dbd316ab9b8466
MdOnim <MdOnim@users.noreply.github.com>
2026-07-08 00:46:20 +0600

    add main layout with navbar and footer

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-cream dark:bg-[#0f1419] transition-colors duration-300">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
